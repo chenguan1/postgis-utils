766f6eeba440c6de2690e9b36def3bee18e16b7c
gray <passer200@163.com>
2020-06-11 23:59:27 +0800

    query ok

--- postgis-utils.go
+++ postgis-utils.go
@@ -2,8 +2,8 @@ package postgis_utils
 
 import (
 	"fmt"
-	"postgis-utils/models"
 	"github.com/jinzhu/gorm"
+	"postgis-utils/models"
 	"strconv"
 	"strings"
 )
@@ -13,7 +13,7 @@ func ReadMetadatas(db *gorm.DB, tableName string) (*models.Metadatas, error) {
 	meta := &models.Metadatas{}
 	meta.TableName = tableName
 
-	if !db.HasTable(tableName){
+	if !db.HasTable(tableName) {
 		return nil, fmt.Errorf("table %v not exist.", tableName)
 	}
 
@@ -23,33 +23,33 @@ func ReadMetadatas(db *gorm.DB, tableName string) (*models.Metadatas, error) {
 	}
 	var fcount FCount
 	err := db.Raw(fmt.Sprintf(`select count(*) from "%v"`, tableName)).Scan(&fcount).Error
-	if err != nil{
+	if err != nil {
 		return nil, fmt.Errorf("ReadMetadatas get feature count failed: %v", err)
 	}
-	if fcount.Count == 0{
-		return nil,fmt.Errorf("ReadMetadatas feature count is 0.")
+	if fcount.Count == 0 {
+		return nil, fmt.Errorf("ReadMetadatas feature count is 0.")
 	}
 	meta.FeatureCount = int(fcount.Count)
 
 	// 字段信息
 	sqlstr := fmt.Sprintf(`SELECT col_description(a.attrelid,a.attnum) as comment,
 format_type(a.atttypid,a.atttypmod) as type,a.attname as name, a.attnotnull as notnull   
-FROM pg_class as c,pg_attribute as a where c.relname = '%v' and a.attrelid = c.oid and a.attnum>0`,tableName)
+FROM pg_class as c,pg_attribute as a where c.relname = '%v' and a.attrelid = c.oid and a.attnum>0`, tableName)
 
 	type FieldInfo struct {
 		Name string
 		Type string
 	}
-	fs := make([]FieldInfo,0,32)
+	fs := make([]FieldInfo, 0, 32)
 	err = db.Raw(sqlstr).Scan(&fs).Error
-	if err != nil{
+	if err != nil {
 		return nil, fmt.Errorf("ReadMetadatas get fields info failed: %v", err)
 	}
 
 	meta.Fields = models.NewFields()
-	for _,v := range fs {
-		meta.Fields.Set(v.Name,v.Type)
-		if strings.Contains(v.Type, "geometry"){
+	for _, v := range fs {
+		meta.Fields.Set(v.Name, v.Type)
+		if strings.Contains(v.Type, "geometry") {
 			meta.GeoColumn = v.Name
 		}
 	}
@@ -63,7 +63,7 @@ FROM pg_class as c,pg_attribute as a where c.relname = '%v' and a.attrelid = c.o
 	sqlstr = fmt.Sprintf(`select ST_SRID(%v), GeometryType(%v) from "%v" limit 1`,
 		meta.GeoColumn, meta.GeoColumn, meta.TableName)
 	err = db.Raw(sqlstr).Scan(&sg).Error
-	if err != nil{
+	if err != nil {
 		return nil, fmt.Errorf("ReadMetadatas get srs and geotype info failed: %v", err)
 	}
 
@@ -75,26 +75,26 @@ FROM pg_class as c,pg_attribute as a where c.relname = '%v' and a.attrelid = c.o
 		St_extent string
 	}
 	var se St_extent
-	sqlstr = fmt.Sprintf(`select ST_Extent("%v") from "%v"`, meta.GeoColumn,meta.TableName)
+	sqlstr = fmt.Sprintf(`select ST_Extent("%v") from "%v"`, meta.GeoColumn, meta.TableName)
 	err = db.Raw(sqlstr).Scan(&se).Error
-	if err != nil{
+	if err != nil {
 		return nil, fmt.Errorf("ReadMetadatas get srs and geotype info failed: %v", err)
 	}
-	strsub := se.St_extent[4:len(se.St_extent)-1]
-	ptStrs := strings.Split(strsub,",")
-	if len(ptStrs) != 2{
+	strsub := se.St_extent[4 : len(se.St_extent)-1]
+	ptStrs := strings.Split(strsub, ",")
+	if len(ptStrs) != 2 {
 		return nil, fmt.Errorf("ReadMetadatas get extent failed: %v", strsub)
 	}
 
-	for i := 0; i < 2; i++{
+	for i := 0; i < 2; i++ {
 		nums := strings.Split(ptStrs[i], " ")
-		meta.Extent[i*2],err = strconv.ParseFloat(nums[0], 64)
-		if err != nil{
+		meta.Extent[i*2], err = strconv.ParseFloat(nums[0], 64)
+		if err != nil {
 			return nil, fmt.Errorf("ReadMetadatas get extent value failed: %v", strsub)
 		}
 
-		meta.Extent[i*2+1],err = strconv.ParseFloat(nums[1], 64)
-		if err != nil{
+		meta.Extent[i*2+1], err = strconv.ParseFloat(nums[1], 64)
+		if err != nil {
 			return nil, fmt.Errorf("ReadMetadatas get extent value failed: %v", strsub)
 		}
 	}
@@ -102,8 +102,169 @@ FROM pg_class as c,pg_attribute as a where c.relname = '%v' and a.attrelid = c.o
 	return meta, nil
 }
 
-// query circle
-func QueryCircel(db *gorm.DB, x, y, r float64)  {
-	
+// query circle, return geojson
+func QueryCircel(db *gorm.DB, tableName string, x, y, r float64) ([]string, error) {
+	sqlstr := fmt.Sprintf(`select st_asgeojson(t.*) as geojson
+		from "%v" as t 
+		where ST_DWithin(
+			ST_Transform(ST_GeomFromText('POINT(%v %v)',4326),26986),
+			ST_Transform(t.geom,26986),
+			%v
+		)`, tableName, x, y, r)
+
+	type GJson struct {
+		Geojson []byte
+	}
+
+	gjs := make([]GJson, 0)
+
+	err := db.Raw(sqlstr).Scan(&gjs).Error
+	if err != nil {
+		return nil, fmt.Errorf("QueryCircel query db failed: %v", err)
+	}
+
+	geojsons := make([]string, 0)
+	for _, v := range gjs {
+		geojsons = append(geojsons, string(v.Geojson))
+	}
+
+	return geojsons, nil
 }
 
+// query rectangle
+func QueryRect(db *gorm.DB, tableName string, minx, miny, maxx, maxy float64) ([]string, error) {
+	sqlstr := fmt.Sprintf(`select st_asgeojson(t.*) as geojson 
+from "%v" as t 
+where ST_Intersects(t.geom, 
+ 'SRID=4326;POLYGON((%v %v,%v %v,%v %v,%v %v))')
+`, tableName, minx, miny, minx, maxy, maxx, maxy, minx, miny)
+
+	type GJson struct {
+		Geojson []byte
+	}
+
+	gjs := make([]GJson, 0)
+
+	err := db.Raw(sqlstr).Scan(&gjs).Error
+	if err != nil {
+		return nil, fmt.Errorf("QueryRect query db failed: %v", err)
+	}
+
+	geojsons := make([]string, 0)
+	for _, v := range gjs {
+		geojsons = append(geojsons, string(v.Geojson))
+	}
+
+	return geojsons, nil
+}
+
+// query polygon
+func QueryPolygon(db *gorm.DB, tableName string, pts []float64) ([]string, error) {
+	if len(pts)%2 != 0 {
+		return nil, fmt.Errorf("QueryPolygon pts length not correct.")
+	}
+
+	ptlist := ""
+	for i, v := range pts {
+		ptlist = ptlist + fmt.Sprintf("%v", v)
+		if i%2 == 0 {
+			ptlist = ptlist + " "
+		} else if i != len(pts)-1 {
+			ptlist = ptlist + ","
+		}
+	}
+
+	sqlstr := fmt.Sprintf(`select st_asgeojson(t.*) as geojson from "%v" as t
+where ST_Intersects(t.geom,'SRID=4326;POLYGON((%v))')`, tableName, ptlist)
+
+	type GJson struct {
+		Geojson []byte
+	}
+
+	gjs := make([]GJson, 0)
+
+	err := db.Raw(sqlstr).Scan(&gjs).Error
+	if err != nil {
+		return nil, fmt.Errorf("QueryRect query db failed: %v", err)
+	}
+
+	geojsons := make([]string, 0)
+	for _, v := range gjs {
+		geojsons = append(geojsons, string(v.Geojson))
+	}
+
+	return geojsons, nil
+}
+
+// query by filed value
+func QueryFiled(db *gorm.DB, tableName, fieldName string, value interface{}, op string) ([]string, error) {
+	sqlstr := fmt.Sprintf(`select st_asgeojson(t.*) as geojson
+		from "%v" as t 
+		where "%v" %v '%v'`, tableName, fieldName, op, value)
+
+	type GJson struct {
+		Geojson []byte
+	}
+
+	gjs := make([]GJson, 0)
+
+	err := db.Raw(sqlstr).Scan(&gjs).Error
+	if err != nil {
+		return nil, fmt.Errorf("QueryFiled query db failed: %v", err)
+	}
+
+	geojsons := make([]string, 0)
+	for _, v := range gjs {
+		geojsons = append(geojsons, string(v.Geojson))
+	}
+
+	return geojsons, nil
+}
+
+// Fuzzy query
+func QueryFuzzy(db *gorm.DB, tableName string, fields []string, keyword string) ([]string, error) {
+	conditions := ``
+	for i, v := range fields{
+		conditions = conditions + fmt.Sprintf(`"%v" like '%v' `, v, "%" + keyword + "%")
+		if i != len(fields)-1 {
+			conditions = conditions + "or "
+		}
+	}
+
+	sqlstr := fmt.Sprintf(`select st_asgeojson(t.*) as geojson
+		from "%v" as t 
+		where %v`, tableName, conditions)
+
+	type GJson struct {
+		Geojson []byte
+	}
+
+	gjs := make([]GJson, 0)
+
+	err := db.Raw(sqlstr).Scan(&gjs).Error
+	if err != nil {
+		return nil, fmt.Errorf("QueryFuzzy query db failed: %v", err)
+	}
+
+	geojsons := make([]string, 0)
+	for _, v := range gjs {
+		geojsons = append(geojsons, string(v.Geojson))
+	}
+
+	return geojsons, nil
+}
+
+// Feature delete
+func FeatureDelete(db *gorm.DB, tableName, idFieldName string, idValue interface{}) error {
+	return nil
+}
+
+// Feature insert
+func FeatureInsert(db *gorm.DB, tableName, featureGeojson string) error {
+	return nil
+}
+
+// Feature update
+func FeatureUpdate(db *gorm.DB, tableName, idFieldName string, idValue interface{}, featureGeojson string) error {
+	return nil
+}
--- postgis-utils_test.go
+++ postgis-utils_test.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_"github.com/jinzhu/gorm/dialects/postgres"
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 )
+
 
 func TestReadMetadatas(t *testing.T) {
 	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
@@ -16,6 +20,7 @@ func TestReadMetadatas(t *testing.T) {
 		t.Error(err)
 		return
 	}
+	defer db.Close()
 
 	meta, err := ReadMetadatas(db, "5ufdxvmgr")
 	if err != nil{
@@ -26,5 +31,117 @@ func TestReadMetadatas(t *testing.T) {
 
 
 
+
+}
+
+func TestQueryCircel(t *testing.T) {
+	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"127.0.0.1", "5432", "postgres", "111111", "dev")
+
+	db, err := gorm.Open("postgres", conn)
+	if err != nil {
+		t.Error(err)
+		return
+	}
 	defer db.Close()
+
+	geojsons, err := QueryCircel(db, "5ufdxvmgr", 120.36965179, 31.48193359, 100000)
+	if err != nil{
+		t.Error(err)
+		return
+	}
+
+	ioutil.WriteFile("data/circle.txt", []byte(strings.Join(geojsons,"\n")), os.ModePerm)
+
+	fmt.Println(len(geojsons))
+}
+
+func TestQueryRect(t *testing.T) {
+	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"127.0.0.1", "5432", "postgres", "111111", "dev")
+
+	db, err := gorm.Open("postgres", conn)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer db.Close()
+
+	geojsons, err := QueryRect(db, "5ufdxvmgr", 118.36965179, 31.48193359, 120.36965179, 32.48193359)
+	if err != nil{
+		println(err)
+		t.Error(err)
+		return
+	}
+
+	ioutil.WriteFile("data/rect.txt", []byte(strings.Join(geojsons,"\n")), os.ModePerm)
+
+	fmt.Println(len(geojsons))
+}
+
+func TestQueryPolygon(t *testing.T) {
+	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"127.0.0.1", "5432", "postgres", "111111", "dev")
+
+	db, err := gorm.Open("postgres", conn)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer db.Close()
+
+	geojsons, err := QueryPolygon(db, "5ufdxvmgr", []float64{118.36965179 ,31.48193359, 120.36965179 ,31.48193359, 120.36965179 ,32.48193359, 119.36965179, 32.48193359, 118.36965179, 31.48193359})
+	if err != nil{
+		println(err)
+		t.Error(err)
+		return
+	}
+
+	ioutil.WriteFile("data/polygon.txt", []byte(strings.Join(geojsons,"\n")), os.ModePerm)
+
+	fmt.Println(len(geojsons))
+}
+
+func TestQueryFiled(t *testing.T) {
+	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"127.0.0.1", "5432", "postgres", "111111", "dev")
+
+	db, err := gorm.Open("postgres", conn)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer db.Close()
+
+	geojsons, err := QueryFiled(db, "5ufdxvmgr", "name", "%北京%", "like")
+	if err != nil{
+		t.Error(err)
+		return
+	}
+
+	ioutil.WriteFile("data/field.txt", []byte(strings.Join(geojsons,"\n")), os.ModePerm)
+
+	fmt.Println(len(geojsons))
+}
+
+func TestQueryFuzzy(t *testing.T) {
+	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"127.0.0.1", "5432", "postgres", "111111", "dev")
+
+	db, err := gorm.Open("postgres", conn)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer db.Close()
+
+	geojsons, err := QueryFuzzy(db, "5ufdxvmgr", []string{"fclass", "name"}, "crossing")
+	if err != nil{
+		t.Error(err)
+		return
+	}
+
+	ioutil.WriteFile("data/fuzzy.txt", []byte(strings.Join(geojsons,"\n")), os.ModePerm)
+
+	fmt.Println(len(geojsons))
 }
